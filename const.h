19b314bd93e040b18ff1148370dd6e494a39c8b9
Softcents Systems <softcents@gmail.com>
2026-09-20 12:44:24 +0600

    Set eSoft Cloud PBX application identity

--- const.h
+++ const.h
@@ -8,5 +8,5 @@
 #define _GLOBAL_VERSION_COMMA 3,22,3,5
 #define _GLOBAL_KEY "*********"
 #define _GLOBAL_VIDEO
-#define _GLOBAL_NAME "BD-PBX"
-#define _GLOBAL_COMPANY "BD PBX"
+#define _GLOBAL_NAME "eSoft-Cloud-PBX"
+#define _GLOBAL_COMPANY "eSoft"
