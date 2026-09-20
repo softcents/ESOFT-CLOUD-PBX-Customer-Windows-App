62a8f818a011344a4dd9f1c0d00b5f70089896cb
Softcents Systems <softcents@gmail.com>
2026-09-20 12:34:09 +0600

    Update eSoft Cloud PBX website and help URLs

--- tools/patch-bdpbx-updates.ps1
+++ tools/patch-bdpbx-updates.ps1
@@ -9,12 +9,12 @@ $checkPattern = '(?s)void CmainDlg::OnCheckUpdates\(\)\s*\{.*?\}\s*\r?\n\s*void 
 $checkReplacement = @'
 void CmainDlg::OnCheckUpdates()
 {
-    // BD PBX: update notifications are permanently disabled.
+    // eSoft Cloud PBX: update notifications are disabled.
 }
 
 void CmainDlg::CheckUpdates()
 {
-    // BD PBX: no remote update check.
+    // eSoft Cloud PBX: no remote update check.
 }
 
 LRESULT CmainDlg::OnUpdateCheckerLoaded
@@ -27,23 +27,21 @@ $websitePattern = '(?s)void CmainDlg::OnMenuWebsite\(\)\s*\{.*?\}\s*\r?\n\s*void
 $websiteReplacement = @'
 void CmainDlg::OnMenuWebsite()
 {
-    MSIP::OpenURL(_T("https://bdpbx.com"));
+    MSIP::OpenURL(_T("https://pbx.esoftbd.net"));
 }
 
 void CmainDlg::OnMenuHelp()
 {
-    MSIP::OpenURL(_T("https://bdpbx.com"));
+    MSIP::OpenURL(_T("https://pbx.esoftbd.net"));
 }
 '@
 $newCpp = [regex]::Replace($cpp, $websitePattern, $websiteReplacement, 1)
 if ($newCpp -eq $cpp) { throw 'Website/help handler block was not found.' }
 $cpp = $newCpp
-
 [IO.File]::WriteAllText($cppPath, $cpp, [Text.UTF8Encoding]::new($false))
 
 $settingsPath = Join-Path $root 'settings.cpp'
 $settings = [IO.File]::ReadAllText($settingsPath)
 $settings = $settings.Replace('GetPrivateProfileString(section, _T("updatesInterval"), NULL, ptr, 256, iniFile);', 'GetPrivateProfileString(section, _T("updatesInterval"), _T("never"), ptr, 256, iniFile);')
 [IO.File]::WriteAllText($settingsPath, $settings, [Text.UTF8Encoding]::new($false))
-
-Write-Host 'BD PBX update checker disabled; Website and Help forced to https://bdpbx.com.'
+Write-Host 'eSoft Cloud PBX update checker disabled; Website and Help forced to https://pbx.esoftbd.net.'
